b4952edd1cacc225a2ae345a41f94b26188a9730
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-22 05:35:48 +0000

    Require an event name to progress

--- pages/api/access.js
+++ pages/api/access.js
@@ -2,6 +2,10 @@ export default async function handler(req, res) {
     const { eventsAirtable } = require('../../lib/airtable')
     try{
         const findCall = await eventsAirtable.find(req.query.item)
+        if(!findCall.fields.Name){
+            res.json({success: false})
+            return
+        }
         res.json({success: true})
     }
     catch(e){
